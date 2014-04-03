c670e69416edd1d1f6a8e29152b46da825970078
Laura <lauradhamilton@gmail.com>
2014-04-03 17:28:47 -0500

    Adding a method that gets server information from the unity server

--- app/models/allscripts_integration.rb
+++ app/models/allscripts_integration.rb
@@ -8,6 +8,7 @@ class AllscriptsIntegration
   @@username = 'AdditiveAnalytics'
   @@password = 'AdditiveAnalytics123!!'
   @@unity_service_url = "http://aws-pehr-13.unitysandbox.com/Unity/UnityService.svc/json"
+  @@appname = 'AdditiveAnalytics.TestApp'
 
   def self.get_security_token
     credentials_to_send = {'Username' => @@username,
@@ -20,4 +21,24 @@ class AllscriptsIntegration
     return response.body
   end
 
+  def self.get_unity_server_info
+    uri = URI.parse(@@unity_service_url + "/MagicJson")
+    http = Net::HTTP.new(uri.host,uri.port)
+    request = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
+    request.body = {'Action' => 'GetServerInfo',
+      'Appname' => @@appname,
+      'AppUserID' => @@username,
+      'PatientID' => '',
+      'Token' => AllscriptsIntegration.get_security_token,
+      'Parameter1' => '',
+      'Parameter2' => '',
+      'Parameter3' => '',
+      'Parameter4' => '',
+      'Parameter5' => '',
+      'Parameter6' => '',
+      'Data' => ''}.to_json
+    puts request.body
+    response = http.request(request)
+    return response.body
+  end
 end
