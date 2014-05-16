b0d84753d249a234dbbb56d3b0bb90d57ac733f6
Laura <lauradhamilton@gmail.com>
2014-05-16 10:43:22 -0500

    Making the price transparency datatables work

--- app/models/medicare_cost_summary.rb
+++ app/models/medicare_cost_summary.rb
@@ -4,11 +4,9 @@ require 'rails'
 class MedicareCostSummary < ActiveRecord::Base
 
   def self.convert_medicare_cost_summary_to_json
-    cost_summary_array = MedicareCostSummary.select([:provider_name, :state]).order('state ASC')
+    cost_summaries = MedicareCostSummary.select([:state, :provider_name, :procedure_type, :procedures_count, :average_cost]).order('state ASC')
     total_records = MedicareCostSummary.count()
-    cost_summary_hash_for_datatables = {"iTotalRecords" => total_records,
-      "iTotalDisplayRecords" => total_records,
-      "aaData" => cost_summary_array}.to_json.html_safe
+    cost_summary_hash_for_datatables = {"aaData" => cost_summaries}
   end
 
 end
