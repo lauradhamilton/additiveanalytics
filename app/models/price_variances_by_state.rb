9b3a3442f8ea169f4ff45072e28dcfccdfd33e54
Laura <lauradhamilton@gmail.com>
2014-05-16 16:01:34 -0500

    Only need state & value

--- app/models/price_variances_by_state.rb
+++ app/models/price_variances_by_state.rb
@@ -3,7 +3,7 @@ require 'rails'
 
 class PriceVariancesByState < ActiveRecord::Base
   def self.convert_price_variance_data_to_json
-    price_variance_hash = PriceVariancesByState.order('state ASC')
+    price_variance_hash = PriceVariancesByState.select(:state, :diff_versus_nationwide_average).order('state ASC')
     return price_variance_hash.to_json
   end
 end
