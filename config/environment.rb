899bd6a303cd0bea0e0c8fd1d2bf66e894f65884
Laura <lauradhamilton@gmail.com>
2014-07-02 16:42:54 -0500

    Fixing config files so the variables are loaded properly

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,7 @@ require File.expand_path('../application', __FILE__)
 Additiveanalytics::Application.initialize!
 
 ALLSCRIPTS_CONFIG = YAML.load_file("#{Rails.root}/config/allscripts.yml")[Rails.env] rescue {}
+
+IMMUNIZATIONS_CONFIG = YAML.load_file("#{Rails.root}/config/immunizations.yml") rescue {}
+
+NEEDED_IMMUNIZATIONS_CONFIG = YAML.load_file("#{Rails.root}/config/sql/find_needed_immunizations.yml") rescue {}
