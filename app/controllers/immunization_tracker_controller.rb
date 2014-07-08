c177a4185585dc07b87d93bf0b8c3204d4fab7f7
Laura <lauradhamilton@gmail.com>
2014-07-08 14:34:03 -0500

    Adding pretty headers to the influenza tracker

--- app/controllers/immunization_tracker_controller.rb
+++ app/controllers/immunization_tracker_controller.rb
@@ -12,9 +12,11 @@ class ImmunizationTrackerController < ApplicationController
     @result.each do |row|
       data << row
     end
+    headers = ["Patient ID", "Gender", "First", "Last", "Email", "Home Phone", "Work Phone", "Cell Phone"]
     xm = Builder::XmlMarkup.new(:indent => 2)
     xm.table {
-      xm.tr { data[0].keys.each { |key| xm.th(key)}}
+      #xm.tr { data[0].keys.each { |key| xm.th(key)}}
+      xm.tr {headers.each { |header| xm.th(header)}}
       data.each { |row| xm.tr { row.values.each { |value| xm.td(value)}}}
     }
     @display_patients_needing_influenza_shots = xm
