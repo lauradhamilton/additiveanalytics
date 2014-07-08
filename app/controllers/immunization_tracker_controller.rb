f19d4cd8476767b3c5bad99e1d5021c4ddb052fb
Laura <lauradhamilton@gmail.com>
2014-07-08 14:26:45 -0500

    Display the patient data w/ an html table

--- app/controllers/immunization_tracker_controller.rb
+++ app/controllers/immunization_tracker_controller.rb
@@ -1,12 +1,22 @@
+require 'builder'
+
 class ImmunizationTrackerController < ApplicationController
   def list
   end
 
   def influenza
+
+    # Generate an HTML table w/ patients needing influenza vaccines
     @result = Patient.find_patients_needing_influenza_shots
-    @display_patients_needing_influenza_shots = []
+    data = []
     @result.each do |row|
-      @display_patients_needing_influenza_shots << row
+      data << row
     end
+    xm = Builder::XmlMarkup.new(:indent => 2)
+    xm.table {
+      xm.tr { data[0].keys.each { |key| xm.th(key)}}
+      data.each { |row| xm.tr { row.values.each { |value| xm.td(value)}}}
+    }
+    @display_patients_needing_influenza_shots = xm
   end
 end
