70733c62f00541d06616c9ed3911a113d1462021
Laura <lauradhamilton@gmail.com>
2014-07-14 10:30:28 -0500

    Explicitly adding the Devise views. Also requiring confirmation of users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :lockable, :timeoutable
+         :confirmable, :lockable, :timeoutable
 
   # This is providers who are allowed to access the patient data
 end
