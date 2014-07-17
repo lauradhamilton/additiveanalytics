a1656495c3b1729b6772e3c554a43523f5827f85
Laura <lauradhamilton@gmail.com>
2014-07-17 17:20:20 -0500

    Adding formatting to the phone numbers

--- app/controllers/immunization_tracker_controller.rb
+++ app/controllers/immunization_tracker_controller.rb
@@ -1,4 +1,5 @@
 require 'builder'
+include ActionView::Helpers::NumberHelper
 
 class ImmunizationTrackerController < ApplicationController
 
@@ -13,13 +14,17 @@ class ImmunizationTrackerController < ApplicationController
     @result = Patient.find_patients_needing_influenza_shots
     data = []
     @result.each do |row|
+      # Display the phone numbers all pretty, with hyphens
+      row["phone_number"] = number_to_phone(row["phone_number"], area_code: true)
+      row["work_phone"]= number_to_phone(row["work_phone"], area_code: true)
+      row["cell_phone"] = number_to_phone(row["cell_phone"], area_code: true)
       data << row
     end
     headers = ["Patient ID", "Gender", "First", "Last", "Email", "Home Phone", "Work Phone", "Cell Phone"]
     cells = data.map do |data|
       "<tr><td>#{data.values.join('</td><td>')}</td></tr>"
     end.join("\n ")
-    table = %{<table style="border-spacing: 10px">
+    table = %{<table style="border-spacing: 5px">
       <tr><th>#{headers.join('</th><th>')}</th></tr>
       #{cells}</table>}
     @display_patients_needing_influenza_shots = table
