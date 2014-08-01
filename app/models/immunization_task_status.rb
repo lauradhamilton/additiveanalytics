2cd34ea394080a50d7d5b69b4b3661e1be3825db
Laura <lauradhamilton@gmail.com>
2014-08-01 09:58:35 -0500

    Renaming the immunization task status model file so that it works properly

--- app/models/immunization_task_status.rb
+++ app/models/immunization_task_status.rb
@@ -1,5 +1,7 @@
+require 'rails'
+
 class ImmunizationTaskStatus < ActiveRecord::Base
-  belongs_to :immunization_tasks
+  belongs_to :immunization_task
 
   enum status: [:postpone_one_day, :postpone_one_month, :postpone_one_year, :close]
 end
