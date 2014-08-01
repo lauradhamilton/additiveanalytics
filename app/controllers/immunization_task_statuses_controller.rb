8cece49f0b10c30518c76538b71952274fc4d214
Laura <lauradhamilton@gmail.com>
2014-08-01 12:17:25 -0500

    Want to save the task status correctly

--- app/controllers/immunization_task_statuses_controller.rb
+++ app/controllers/immunization_task_statuses_controller.rb
@@ -9,7 +9,7 @@ class ImmunizationTaskStatusesController < ApplicationController
 
     @immunization_task_status.immunization_task_id = params[:immunization_task_id]
     @immunization_task_status.user_id = current_user.id
-    @immunization_task_status.status = :close
+    @immunization_task_status.status = "close"
 
     @immunization_task_status.save!
   end
