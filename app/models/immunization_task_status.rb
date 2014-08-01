29f7940bce9f3cbaad806c7ac451ac110a704be6
Laura <lauradhamilton@gmail.com>
2014-08-01 10:26:04 -0500

    Adding validations to immunization_task_status

--- app/models/immunization_task_status.rb
+++ app/models/immunization_task_status.rb
@@ -4,4 +4,7 @@ class ImmunizationTaskStatus < ActiveRecord::Base
   belongs_to :immunization_task
 
   enum status: [:postpone_one_day, :postpone_one_month, :postpone_one_year, :close]
+
+  validates :immunization_task_id, presence: true
+  validates :user_id, presence: true
 end
