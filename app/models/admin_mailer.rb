aaf9ffaf6ef50091191adf95f1afe8ce55448b72
Laura <lauradhamilton@gmail.com>
2014-08-04 12:08:41 -0500

    Deleting adminmailer

--- app/models/admin_mailer.rb
+++
@@ -1,9 +0,0 @@
-class AdminMailer < ActionMailer::Base
-  default to: Proc.new {'laura@additiveanalytics.com'},
-          from: 'support@additiveanalytics.com'
- 
-  def new_user_waiting_for_approval(user)
-    @user = user
-    mail(subject: "New User Signup: #{@user.email}")
-  end
-end
