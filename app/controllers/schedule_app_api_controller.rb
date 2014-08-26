b9b2fd852a8c2f816c8354c9845ae42d32c12202
Laura <lauradhamilton@gmail.com>
2014-08-26 12:51:36 -0500

    RESTful API to display today's appointments

--- app/controllers/schedule_app_api_controller.rb
+++ app/controllers/schedule_app_api_controller.rb
@@ -1,4 +1,6 @@
 class ScheduleAppApiController < ActionController::API
   def index
+    today_appointments = PatientAppointment.where("appointment_time >= ?", Date.today).where("appointment_time <= ?", Date.tomorrow)
+    render json: today_appointments
   end
 end
