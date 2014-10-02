ef07ffb732644d41960a93e7507a65801e2145f5
Laura <lauradhamilton@gmail.com>
2014-10-02 09:40:23 -0500

    Fixing date/timestamp bug in schedule app api

--- app/controllers/schedule_app_api_controller.rb
+++ app/controllers/schedule_app_api_controller.rb
@@ -13,7 +13,7 @@ class ScheduleAppApiController < ActionController::Base
   def index
     appointment_date = Date.strptime(params[:appointment_date], '%Y%m%d')
     attending_abbreviation = params[:attending_abbreviation]
-    appointment_list = PatientAppointment.where("appointment_time >= ?", appointment_date).where("appointment_time <= ?", appointment_date).where("attending_abbreviation = ?", attending_abbreviation)
+    appointment_list = PatientAppointment.where("appointment_time >= ?", appointment_date).where("appointment_time < ?", appointment_date.next).where("attending_abbreviation = ?", attending_abbreviation)
     render json: appointment_list
     AuditLog.record_schedule_app_audit(current_user.id) # Make a record that the API was called
     # This is needed for HIPAA
